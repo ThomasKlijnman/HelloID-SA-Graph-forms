cab17a43847efa48701a03aee08daaca7457b1f3
ThatGraphCodeArtisan <15871270+ThomasKlijnman@users.noreply.github.com>
2023-12-21 17:06:11 +0100

    Update DS1-GetAllUsersSelectedAttributes.ps1
    Signed-off-by: ThatGraphCodeArtisan <15871270+ThomasKlijnman@users.noreply.github.com>

--- EntraID-Modify-UserGroupMemberships/DS1-GetAllUsersSelectedAttributes.ps1
+++ EntraID-Modify-UserGroupMemberships/DS1-GetAllUsersSelectedAttributes.ps1
@@ -1,8 +1,8 @@
-
+# Define the search value and query
 $searchValue = $datasource.searchUser
 $searchQuery = "*$searchValue*"
 
-# Bouw de autorisatieheader
+# Build the authorization header
 $tokenUrl = "https://login.microsoftonline.com/$GraphtenantID/oauth2/token"
 $body = @{
     'resource' = 'https://graph.microsoft.com'
@@ -11,47 +11,65 @@ $body = @{
     'grant_type' = 'client_credentials'
 }
 
-# Verkrijg het access token
+# Obtain the access token
 $tokenResponse = Invoke-RestMethod -Method Post -Uri $tokenUrl -ContentType "application/x-www-form-urlencoded" -Body $body
 $accessToken = $tokenResponse.access_token
 
-# Voer het GET-verzoek uit met filtering direct in de URL
-$authorization = @{
-    'Authorization' = "Bearer $accessToken"
-    'Content-Type' = 'application/json'
+# Define the function to invoke the Graph API
+function Invoke-GraphApi {
+    param (
+        [string]$ApiEndpoint,
+        [string]$Method = 'GET',
+        [string]$Body = $null,
+        [string]$Version = 'v1.0'
+    )
+
+    # Set the base URL for the Graph API
+    $baseUrl = "https://graph.microsoft.com/$Version"
+
+    # Replace 'YOUR_ACCESS_TOKEN' with the dynamically obtained access token
+    $authorization = @{
+        'Authorization' = "Bearer $accessToken"
+        'Content-Type' = 'application/json'
+    }
+
+    # Build the full URL
+    $url = "$baseUrl$ApiEndpoint"
+
+    # Execute the HTTP call based on the specified method
+    $response = Invoke-RestMethod -Uri $url -Headers $authorization -Method $Method -Body $Body -ContentType "application/json"
+
+    # Return the response
+    $response
 }
 
+# Define the properties to be selected
 $properties = @("displayName","userPrincipalName","mail","accountEnabled","createdDateTime","id")
 
-$baseSearchUri = "https://graph.microsoft.com/"
-$searchUri = $baseSearchUri + "v1.0/users" + '?$select=' + ($properties -join ",") + '&$top=999'
+# Define the endpoint for fetching users
+$userEndpoint = '/users' + '?$select=' + ($properties -join ",") + '&$top=999'
+$EntraIDUsersResponse = Invoke-GraphApi -ApiEndpoint $userEndpoint
 
-Try{
-  $EntraIDUsersResponse = Invoke-RestMethod -Uri $searchUri -Method Get -Headers $authorization -Verbose:$false
-  $EntraIDUsers = $EntraIDUsersResponse.value
+# Initialize the user list
+$EntraIDUsers = $EntraIDUsersResponse.value
 
-  while (![string]::IsNullOrEmpty($EntraIDUsersResponse.'@odata.nextLink')) {
-    $EntraIDUsersResponse = Invoke-RestMethod -Uri $EntraIDUsersResponse.'@odata.nextLink' -Method Get -Headers $authorization -Verbose:$false
+# Loop through the paginated results
+while (![string]::IsNullOrEmpty($EntraIDUsersResponse.'@odata.nextLink')) {
+    $EntraIDUsersResponse = Invoke-GraphApi -ApiEndpoint $EntraIDUsersResponse.'@odata.nextLink'
     $EntraIDUsers += $EntraIDUsersResponse.value
-  }  
+}  
 
-  if($EntraIDUsersResponse.count -gt 0){
+# If there are users, loop through each user and output its properties if the displayName matches the search query
+if($EntraIDUsersResponse.count -gt 0){
     foreach($user in $EntraIDUsers){
       if($user.displayName -like $searchQuery){
           $returnObject = @{
              displayName=$user.displayName;
-              userPrincipalName=$user.userPrincipalName;
-             #mail=$user.mail;
+             userPrincipalName=$user.userPrincipalName;
              accountEnabled=$user.accountEnabled
-            GUID=$user.id
-        }
-      Write-Output $returnObject
+             GUID=$user.id
+          }
+        Write-Output $returnObject
       }
    }
- }
 }
-catch {
-        $errorDetailsMessage = ($_.ErrorDetails.Message | ConvertFrom-Json).error.message
-        Write-Error ("Error searching for EntraID Users. Error: $_" + $errorDetailsMessage)
-}
-
